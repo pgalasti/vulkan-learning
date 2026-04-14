4e1f372208803e94c56a7a6f3f82534667b1d704
Paul Galasti <pgalasti@gmail.com>
2026-04-13 21:21:43 -0400

    Empty window + vulkan setup

---
+++ 01_triangle/main.cpp
@@ -0,0 +1,71 @@
+#include <memory>
+#include <vulkan/vulkan_raii.hpp>
+#include <GLFW/glfw3.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+const uint32_t WIDTH  = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication
+{
+  public:
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+  private:
+	GLFWwindow *window = nullptr;
+
+	void initWindow()
+	{
+		glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan()
+	{
+	}
+
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
+
+	void cleanup()
+	{
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
+	}
+};
+
+int main()
+{
+	try
+	{
+		HelloTriangleApplication app;
+		app.run();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
